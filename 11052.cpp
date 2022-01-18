f963fe92d661ce66995569963ea2c084438625c6
ssssujini99 <sssssjin99@naver.com>
2022-01-18 18:25:48 +0900

    11052_카드 구매하기

---
+++ 11052.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int d[1001]={0, };
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n, tmp, res, cnt=0;
+    cin>>n;
+    cin>>tmp;
+    d[1]=tmp;
+
+    for(int i=2; i<=n; i++){
+      cin>>tmp;
+      d[i]=tmp;
+      for(int j=i-1; j>=i/2; j--){
+        d[i]=max(d[i], d[j]+d[i-j]);
+      }
+    }
+
+    cout<<d[n]<<endl;
+    
+    return 0;
+}
