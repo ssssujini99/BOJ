63638e89acded9f7f1015131ea546c08e472123c
수지니 <71487608+ssssujini99@users.noreply.github.com>
2022-02-09 02:47:04 +0900

    15654_N과 M (5)

---
+++ 15654.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, m;
+int ch[10001]={0, }; // 중복 여부 확인 배열
+int res[9]={0, }; // 결과 배열
+vector<int> v;
+
+void DFS(int idx){
+  if(idx==m+1){
+    for(int i=1; i<=m; i++) cout<<res[i]<<" ";
+    cout<<"\n";
+  }
+  else{
+    for(int i=0; i<n; i++){
+      if(!ch[v[i]]){
+        ch[v[i]]=1;
+        res[idx]=v[i];
+        DFS(idx+1);
+        ch[v[i]]=0;
+      }
+    }
+  }
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    cin>>n>>m;
+    for(int i=0; i<n; i++){
+      int tmp; cin>>tmp;
+      v.push_back(tmp);
+    }
+    sort(v.begin(), v.end());
+    DFS(1);
+
+    return 0;
+}
